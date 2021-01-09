ed959400242ab3fbf3dfd1702872795e22cbd2fc
hayato sugawara <hayato711@gmail.com>
2021-01-09 22:40:09 +0900

    until basics-7

--- main.go
+++ main.go
@@ -4,10 +4,19 @@ import (
 	"fmt"
 )
 
-func add(x int, y int) int {
-	return x + y
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
+func split(sum int) (x, y int) {
+	x = sum * 4 / 9
+	y = sum - x
+	return
 }
 
 func main() {
-	fmt.Println(add(24, 55))
+	a, b := swap("hey", "world")
+	fmt.Println(a, b)
+
+	fmt.Println(split(5))
 }
