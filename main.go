ee27bc583602dcd455c5603a545efd6811197bf3
hayato sugawara <hayato711@gmail.com>
2021-01-10 12:54:27 +0900

    until moretypes-25

--- main.go
+++ main.go
@@ -1,18 +1,22 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-type Vertex struct {
-	Lat, Long float64
-}
-
-var m = map[string]Vertex{
-	"Bell Labs": {40.5451, -76.5452},
-	"Google":    {34.344, 43.3443},
+func adder() func(int) int {
+	sum := 0
+	return func(x int) int {
+		sum += x
+		return sum
+	}
 }
 
 func main() {
-	fmt.Println(m)
+	pos, neg := adder(), adder()
+
+	for i := 0; i < 10; i++ {
+		fmt.Println(
+			pos(i),
+			neg(-2*i),
+		)
+	}
 }
