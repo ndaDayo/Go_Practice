3aff12f3226341777fa9913f834b82b292188858
hayato sugawara <hayato711@gmail.com>
2021-01-16 23:09:59 +0900

    method for rangeの説明

--- Chap3.2/method01/main.go
+++ Chap3.2/method01/main.go
@@ -6,6 +6,12 @@ func main() {
 	fs := make([]func() string, 2)
 	fs[0] = func() string { return "nda" }
 	fs[1] = func() string { return "dayo" }
+	// for i, f := range fs {
+	// 	fmt.Println(i)
+	// 	fmt.Println(f())
+	// }
+
+	// keyは _ で書いておけば、valueだけ取得できる
 	for _, f := range fs {
 		fmt.Println(f())
 	}
